706689c9aba9c46c0f36647e1f0d07fa18beed64
Fleming John <flemjohn08@gmail.com>
2025-10-12 07:27:00 +0000

    add an redirect to landing page

--- src/components/codetube/Header.tsx
+++ src/components/codetube/Header.tsx
@@ -1,10 +1,11 @@
 import { Clapperboard } from 'lucide-react';
+import Link from 'next/link';
 
 export default function Header() {
   return (
-    <div className="flex items-center gap-2">
+    <Link href="/" className="flex items-center gap-2">
       <Clapperboard className="w-8 h-8 text-primary" />
       <h1 className="text-2xl font-bold font-headline text-white">CodeTube</h1>
-    </div>
+    </Link>
   );
 }
